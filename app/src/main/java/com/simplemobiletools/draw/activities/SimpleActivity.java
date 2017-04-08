e207ef69549388e3da0adc9f28217d2ce9594e9d
tibbi <tibor@kaputa.sk>
2017-04-08 18:37:18 +0200

    add a splash screen

--- app/src/main/java/com/simplemobiletools/draw/activities/SimpleActivity.java
+++
@@ -1,7 +0,0 @@
-package com.simplemobiletools.draw.activities;
-
-import com.simplemobiletools.commons.activities.BaseSimpleActivity;
-
-public class SimpleActivity extends BaseSimpleActivity {
-
-}
---
+++ app/src/main/kotlin/com/simplemobiletools/draw/activities/SimpleActivity.kt
@@ -0,0 +1,5 @@
+package com.simplemobiletools.draw.activities
+
+import com.simplemobiletools.commons.activities.BaseSimpleActivity
+
+open class SimpleActivity : BaseSimpleActivity()
