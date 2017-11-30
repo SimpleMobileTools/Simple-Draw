7bc3fb719729fb534f2575ab0f9f8f0893444c60
tibbi <tibor@kaputa.sk>
2017-11-30 19:36:36 +0100

    update commons to 3.0.12

--- app/src/main/kotlin/com/simplemobiletools/draw/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/draw/App.kt
@@ -1,10 +1,9 @@
 package com.simplemobiletools.draw
 
 import android.app.Application
+import com.simplemobiletools.commons.extensions.checkUseEnglish
 import com.simplemobiletools.draw.BuildConfig.USE_LEAK_CANARY
-import com.simplemobiletools.draw.extensions.config
 import com.squareup.leakcanary.LeakCanary
-import java.util.*
 
 class App : Application() {
     override fun onCreate() {
@@ -16,10 +15,6 @@ class App : Application() {
             LeakCanary.install(this)
         }
 
-        if (config.useEnglish) {
-            val conf = resources.configuration
-            conf.locale = Locale.ENGLISH
-            resources.updateConfiguration(conf, resources.displayMetrics)
-        }
+        checkUseEnglish()
     }
 }
