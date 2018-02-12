d70fe4e2811073258c6abf73dcbd2f8cb4ea8186
tibbi <tibor@kaputa.sk>
2018-02-12 15:58:38 +0100

    add a toggle for hiding Whats New

--- app/src/main/kotlin/com/simplemobiletools/draw/activities/SettingsActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/draw/activities/SettingsActivity.kt
@@ -20,6 +20,7 @@ class SettingsActivity : SimpleActivity() {
 
         setupCustomizeColors()
         setupUseEnglish()
+        setupAvoidWhatsNew()
         setupBrushSize()
         updateTextColors(settings_holder)
     }
@@ -37,6 +38,14 @@ class SettingsActivity : SimpleActivity() {
             settings_use_english.toggle()
             config.useEnglish = settings_use_english.isChecked
             useEnglishToggled()
+        }
+    }
+
+    private fun setupAvoidWhatsNew() {
+        settings_avoid_whats_new.isChecked = config.avoidWhatsNew
+        settings_avoid_whats_new_holder.setOnClickListener {
+            settings_avoid_whats_new.toggle()
+            config.avoidWhatsNew = settings_avoid_whats_new.isChecked
         }
     }
 
