154793aef04c294aec412b46ac5b868565d1afe0
tibbi <tibor@kaputa.sk>
2018-02-27 20:40:32 +0100

    update commons to 3.13.16

--- app/src/main/kotlin/com/simplemobiletools/draw/helpers/Constants.kt
+++ app/src/main/kotlin/com/simplemobiletools/draw/helpers/Constants.kt
@@ -5,7 +5,6 @@ const val CANVAS_BACKGROUND_COLOR = "canvas_background_color"
 const val SHOW_BRUSH_SIZE = "show_brush_size"
 const val BRUSH_SIZE = "brush_size"
 const val LAST_SAVE_FOLDER = "last_save_folder"
-const val PREVENT_PHONE_FROM_SLEEPING = "prevent_phone_from_sleeping"
 
 const val PNG = "png"
 const val SVG = "svg"
