657ff0b68505c56be491efc3ecbb64f2e9d5f22f
tibbi <tibor@kaputa.sk>
2018-08-26 18:16:09 +0200

    removing the Leak Canary license

--- app/src/main/kotlin/com/simplemobiletools/draw/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/draw/activities/MainActivity.kt
@@ -15,7 +15,6 @@ import android.widget.SeekBar
 import com.simplemobiletools.commons.dialogs.ColorPickerDialog
 import com.simplemobiletools.commons.dialogs.FilePickerDialog
 import com.simplemobiletools.commons.extensions.*
-import com.simplemobiletools.commons.helpers.LICENSE_LEAK_CANARY
 import com.simplemobiletools.commons.helpers.PERMISSION_WRITE_STORAGE
 import com.simplemobiletools.commons.models.FAQItem
 import com.simplemobiletools.commons.models.FileDirItem
@@ -134,7 +133,7 @@ class MainActivity : SimpleActivity(), CanvasListener {
     }
 
     private fun launchAbout() {
-        val licenses = LICENSE_LEAK_CANARY
+        val licenses = 0
 
         val faqItems = arrayListOf(
                 FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons)
