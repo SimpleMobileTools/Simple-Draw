d03a287a625249cd33958021bbfb6b0b077686e2
tibbi <tibor@kaputa.sk>
2018-09-01 23:12:11 +0200

    adding optional zoom in release notes

--- app/src/main/kotlin/com/simplemobiletools/draw/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/draw/activities/MainActivity.kt
@@ -457,6 +457,7 @@ class MainActivity : SimpleActivity(), CanvasListener {
         arrayListOf<Release>().apply {
             add(Release(18, R.string.release_18))
             add(Release(20, R.string.release_20))
+            add(Release(38, R.string.release_38))
             checkWhatsNew(this, BuildConfig.VERSION_CODE)
         }
     }
