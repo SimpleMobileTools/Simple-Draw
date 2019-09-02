d7210a7780d370ce85f881d85342ee750794c28d
tibbi <tibor@kaputa.sk>
2019-09-02 21:35:38 +0200

    do not change savedPathsHash on rotation change afterall

--- app/src/main/kotlin/com/simplemobiletools/draw/pro/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/draw/pro/activities/MainActivity.kt
@@ -501,7 +501,6 @@ class MainActivity : SimpleActivity(), CanvasListener {
             uriToLoad = Uri.parse(savedInstanceState.getString(URI_TO_LOAD))
             tryOpenUri(uriToLoad!!, intent)
         }
-        savedPathsHash = my_canvas.getDrawingHashCode()
     }
 
     private var onStrokeWidthBarChangeListener: SeekBar.OnSeekBarChangeListener = object : SeekBar.OnSeekBarChangeListener {
