fd6548c08da4590e0b246cdefcaac10c5bad6264
Naveen <snaveen935@gmail.com>
2023-03-11 06:52:42 +0530

    Remove the original Flood-fill implementation

--- app/src/main/kotlin/com/simplemobiletools/draw/pro/extensions/Bitmap.kt
+++ app/src/main/kotlin/com/simplemobiletools/draw/pro/extensions/Bitmap.kt
@@ -1,19 +1,8 @@
 package com.simplemobiletools.draw.pro.extensions
 
 import android.graphics.Bitmap
-import com.simplemobiletools.draw.pro.helpers.QueueLinearFloodFiller
 import com.simplemobiletools.draw.pro.helpers.VectorFloodFiller
 import com.simplemobiletools.draw.pro.models.MyPath
-
-fun Bitmap.floodFill(color: Int, x: Int, y: Int, tolerance: Int = 10): Bitmap {
-    val floodFiller = QueueLinearFloodFiller(this).apply {
-        fillColor = color
-        setTolerance(tolerance)
-    }
-
-    floodFiller.floodFill(x, y)
-    return floodFiller.image!!
-}
 
 fun Bitmap.vectorFloodFill(color: Int, x: Int, y: Int, tolerance: Int): MyPath {
     val floodFiller = VectorFloodFiller(this).apply {
